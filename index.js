54b38a5b0b8253244896c1adcfb55c7a4b2f9ffd
quip550@gmail.com Chi <quip550@gmail.com>
2018-05-20 18:01:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Joe"
 var height = 74
-var message = 'is  inches tall`
+var message = "is inches tall"
 console.log(name)
 module.exports = {name, height, message}
